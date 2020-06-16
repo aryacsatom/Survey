5a1bf9edde444e00ff726fa3320ebf9c4fe3d773
Arya Abhishek <35173681+aryacsatom@users.noreply.github.com>
2020-06-16 11:50:11 +0530

    Update SurveyController.js

--- src/controllers/SurveyController.js
+++ src/controllers/SurveyController.js
@@ -1,7 +1,7 @@
 const Household=require('../models/Household');
 const LocalAsset=require('../models/LocalAsset');
 
-exports.household=(req,res)=>{//Client
+/*exports.household=(req,res)=>{//Client
     const houseHold = new Household(req.body);
     houseHold.save()
     .then(user => {
@@ -29,33 +29,39 @@ exports.localasset=(req,res)=>{//Client
         var message={error:true,err:err};
         res.json(message);
     })
-}
+}*/
 
-exports.survey=(req,res)=>{
-    const {localasset,household}=req.body
+exports.survey=async (req,res)=>{
+    try{
+    const {household,localasset}=req.body
     const localAsset=new LocalAsset(localasset)
-    const houseHold=new HouseHold(household)
-    localAsset.save()
-    .then(user => {
+    const houseHold=new Household(household)
+    const la=await localAsset.save()
+    /*.then(user => {
         // console.log(user);
-        var message={error:false,user: user};
+        var message={error:false,msg:"LocalAsset Survey Saved"};
         res.json(message);
     })
     .catch(err => {
         // console.log(err);
         var message={error:true,err:err};
         res.json(message);
-    })
+    })*/
 
-    houseHold.save()
-    .then(user => {
+    const hh=await houseHold.save()
+    var message={error:false,msg:"LocalAsset Survey Saved",hh,la};
+    res.status(200).send(message)
+    /*.then(user => {
         // console.log(user);
-        var message={error:false,user: user};
+        var message={error:false,msg:"Household and Local Survey Saved"};
         res.json(message);
     })
     .catch(err => {
         // console.log(err);
         var message={error:true,err:err};
         res.json(message);
-    })
+    })*/
+}catch(err){
+    res.status(500).send(err);
+}
 }
