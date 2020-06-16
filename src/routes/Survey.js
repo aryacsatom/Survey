db51802de8f9d10f29f03adc78bade51401fafb7
Arya Abhishek <35173681+aryacsatom@users.noreply.github.com>
2020-06-16 11:21:57 +0530

    Update Survey.js

--- src/routes/Survey.js
+++ src/routes/Survey.js
@@ -2,8 +2,9 @@ const router=require('express').Router();
 const SurveyController=require('../controllers/SurveyController');
 const auth=require('../middleware/auth')
 
-router.post('/Household',auth,SurveyController.household);
-router.post('/LocalAsset',auth,SurveyController.localasset);
+//router.post('/Household',auth,SurveyController.household);
+//router.post('/LocalAsset',auth,SurveyController.localasset);
+router.post('/Survey',auth,SurveyController.survey)
 
 
 module.exports=router;
